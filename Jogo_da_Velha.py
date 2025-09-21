dbcbe476d78a89093ed229ae6b57161db646f581
Altairi40 <lucas.shiraoka@gmail.com>
2025-09-21 11:39:49 -0300

    Pronto, agora está completo e funcionando o Jogo da Velha e a solução para isso foi colocar os comandos da vez dentro de um 'if', se o tabuleiro[linha][coluna] for 'diferente' de X ou O, ou seja, estiver vazio, então a jogada será feita normalmente, senão uma mensagem de 'Jogada inváida, tente denovo' vai aparecer e o jogador em questão repete a jogada, sem adicionar um valor na variável 'jogadas' para não dar empate antes da hora

--- Jogo_da_Velha.py
+++ Jogo_da_Velha.py
@@ -11,42 +11,42 @@ def vitoria(rodada): #Função gigante que garante a vitória pra quem completar
     global stop
     if(tabuleiro[0][0] == rodada and tabuleiro[0][1] == rodada and tabuleiro[0][2] == rodada):
         interface()
-        print("O {} Venceu".format(rodada))
+        print("O '{}' Venceu".format(rodada))
         stop = True
     
     if(tabuleiro[1][0] == rodada and tabuleiro[1][1] == rodada and tabuleiro[1][2] == rodada):
         interface()
-        print("O {} Venceu".format(rodada))
+        print("O '{}' Venceu".format(rodada))
         stop = True
     
     if(tabuleiro[2][0] == rodada and tabuleiro[2][1] == rodada and tabuleiro[2][2] == rodada):
         interface()
-        print("O {} Venceu".format(rodada))
+        print("O '{}' Venceu".format(rodada))
         stop = True
 
     if(tabuleiro[0][0] == rodada and tabuleiro[1][0] == rodada and tabuleiro[2][0] == rodada):
         interface()
-        print("O {} Venceu".format(rodada))
+        print("O '{}' Venceu".format(rodada))
         stop = True
     
     if(tabuleiro[0][1] == rodada and tabuleiro[1][1] == rodada and tabuleiro[1][1] == rodada):
         interface()
-        print("O {} Venceu".format(rodada))
+        print("O '{}' Venceu".format(rodada))
         stop = True
     
     if(tabuleiro[0][2] == rodada and tabuleiro[1][2] == rodada and tabuleiro[2][2] == rodada):
         interface()
-        print("O {} Venceu".format(rodada))
+        print("O '{}' Venceu".format(rodada))
         stop = True
 
     if(tabuleiro[0][0] == rodada and tabuleiro[1][1] == rodada and tabuleiro[2][2] == rodada):
         interface()
-        print("O {} Venceu".format(rodada))
+        print("O '{}' Venceu".format(rodada))
         stop = True
 
     if(tabuleiro[2][0] == rodada and tabuleiro[1][1] == rodada and tabuleiro[0][2] == rodada):
         interface()
-        print("O {} Venceu".format(rodada))
+        print("O '{}' Venceu".format(rodada))
         stop = True
     
 
@@ -67,15 +67,23 @@ while stop == False:
     coluna = int(input("Digite o número da coluna: "))
 
     if rodada == "X":
-        tabuleiro[linha][coluna] = "X"
-        vitoria(rodada)
-        jogadas += 1
-        rodada = "O"
+        if tabuleiro[linha][coluna] != "O" and tabuleiro[linha][coluna] != "X":
+            tabuleiro[linha][coluna] = "X"
+            vitoria(rodada)
+            jogadas += 1
+            rodada = "O"
+        else:
+            print("\nJogada inváida, tente denovo")
+            jogadas += 0
 
     # Vai ficar nessa troca infinita até o empate ou a "vitoria"
     elif rodada == "O":
-        tabuleiro[linha][coluna] = "O"
-        vitoria(rodada)
-        jogadas += 1
-        rodada = "X"
+        if tabuleiro[linha][coluna] != "O" and tabuleiro[linha][coluna] != "X":
+            tabuleiro[linha][coluna] = "O"
+            vitoria(rodada)
+            jogadas += 1
+            rodada = "X"
+        else:
+            print("\nJogada inváida, tente denovo")
+            jogadas += 0
 
